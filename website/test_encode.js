57ca6ce1a9cf912c5b70e5fa4a3b742142474ebf
Ijaz <ijazabd4333@gmail.com>
2026-06-10 18:31:41 +0500

    Remove mobile app promo banner, enlarge payment badges, and delete unnecessary files

--- website/test_encode.js
+++
@@ -1,1 +0,0 @@
-const v = "['|',('id','=',4832),('parent_id','=',4832)]"; const enc = encodeURIComponent(v).replace(/%5B/gi, '[').replace(/%5D/gi, ']').replace(/%2C/gi, ',').replace(/'/g, '%27'); console.log(enc);
